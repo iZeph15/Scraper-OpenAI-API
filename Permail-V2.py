008592f48ddc24eabf50326217e92a2e04ebc2e9
iZeph15 <vz.gagarin@gmail.com>
2023-02-21 21:50:58 +0800

    Update Permail-V2.py

--- Permail-V2.py
+++ Permail-V2.py
@@ -1,20 +1,20 @@
 import csv
 import openai
 
-openai.api_key = ""
+openai.api_key = "#" #You can't go anywhere without this. 
 
-with open('website_descriptions.csv', 'r', encoding='utf-8') as csv_file:
+with open('test.csv', 'r', encoding='utf-8') as csv_file: #Now this is where the trainer file goes in. 
     csv_reader = csv.reader(csv_file)
 
-    with open('results.csv', 'w', encoding='utf-8', newline='') as results_file:
+    with open('results.csv', 'w', encoding='utf-8', newline='') as results_file: #What's a data without clean output right? This line fixes the weird stuffs inside the csv.
         csv_writer = csv.writer(results_file, delimiter='|')
 
-        for row in csv_reader:
-            website_description = row[0]
-            response = openai.Completion.create(
-                engine="text-davinci-003",
-                prompt=(f"Basically, you just need to put stuffs in here. {website_description}"),
-                max_tokens=50,
+        for row in csv_reader: #loops on the headers, impt.
+            website_description = row[0] #Reads the first row, counts starts -> 1
+            response = openai.Completion.create( #Now, check the docs. These can be replaced based on your goal. But in this case. This will call the api to create a a response in the form of string.
+                engine="text-davinci-003", #You can also use davinci-002 or 1. 003 is beneficial and is much faster compared to the lesser versions.
+                prompt=(f"Basically, you just need to put stuffs in here. {test_row}"),  #This is the goal, to give a response similar to chatgpt.
+                max_tokens=50, #Play with these settings based on your budget.
                 n = 1,
                 stop=None,
                 temperature=0.7,
@@ -23,7 +23,7 @@ with open('website_descriptions.csv', 'r', encoding='utf-8') as csv_file:
                 top_p = 1,
                 presence_penalty = 0
             )
-            csv_writer.writerow([response["choices"][0]["text"].replace("\n", " ")])
+            csv_writer.writerow([response["choices"][0]["text"].replace("\n", " ")]) #This is where you save the stuffs the AI gives. 
             
-#I changed the directory and API to number sign for privacy.
+#I changed the API to number sign for privacy.
 #I played around with the penalty settings to see which will generate better results
